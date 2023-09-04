86556d70b7fda0d3d0a2fff2967d543acc47d23e
Phillipe Wolff <phillipe.wolff@gmail.com>
2023-09-04 18:45:04 -0300

    projeção dos dados do autor com DTO

--- workshopmongodb/src/main/java/com/wolffcode/workshopmongodb/entities/Post.java
+++ workshopmongodb/src/main/java/com/wolffcode/workshopmongodb/entities/Post.java
@@ -1,5 +1,7 @@
 package com.wolffcode.workshopmongodb.entities;
 
+import com.wolffcode.workshopmongodb.dto.AuthorDTO;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
@@ -9,12 +11,12 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -22,11 +24,11 @@ public class Post implements Serializable {
         this.author = author;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
