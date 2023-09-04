c7a58677d83ba6d1e650c3a57e1ca6c36866009b
Phillipe Wolff <phillipe.wolff@gmail.com>
2023-09-04 20:58:48 -0300

    ref user posts

--- workshopmongodb/src/main/java/com/wolffcode/workshopmongodb/entities/User.java
+++ workshopmongodb/src/main/java/com/wolffcode/workshopmongodb/entities/User.java
@@ -1,9 +1,12 @@
 package com.wolffcode.workshopmongodb.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -13,9 +16,8 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
-
-
-    private Post post;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     private Comment comment;
 
@@ -26,6 +28,14 @@ public class User implements Serializable {
         this.id = id;
         this.name = name;
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     public String getId() {
